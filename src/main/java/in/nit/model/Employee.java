ac5c64d37e2eb352c456810d973346ea622c67be
Sameera <Sameera@LAPTOP-G7GDIT28>
2020-03-01 16:23:05 +0530

    hi hello 5454

--- src/main/java/in/nit/model/Employee.java
+++ src/main/java/in/nit/model/Employee.java
@@ -8,7 +8,7 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Data
+
 @Table(name="emp")
 public class Employee {
 	@Id
@@ -18,4 +18,41 @@ public class Employee {
 	private String empName;
 	@Column
 	private Double empSal;
+	
+	public Employee() {
+		super();
+	}
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Double getEmpSal() {
+		return empSal;
+	}
+
+	public void setEmpSal(Double empSal) {
+		this.empSal = empSal;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
+	}
+	
+	
+	
+	
 }
